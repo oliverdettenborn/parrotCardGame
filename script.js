3ca6aac1259c553881cddb59ae99a1a7bb58f951
thaliard <thalia.born@gmail.com>
2020-09-09 15:50:54 -0300

    função de verificação da quantidade de cartas escolhida é par, maior que 4 e menor que 14

--- script.js
+++ script.js
@@ -21,6 +21,10 @@ ao final do jogo informar o numero de jogadas -> numero de cliques / 2
 function iniciar(){
     var qtdCartas = parseInt(prompt("Quantas cartas você quer jogar? \n Escolha um nº entre 4 e 14"));
 
-    
+    // verificação se o nº é par, maior que quatro e menor que 14
+    while((qtdCartas % 2 !== 0) || (qtdCartas < 4) || (qtdCartas > 14)){
+        qtdCartas = parseInt(prompt("Você precisa escolher um nº par entre 4 e 14. Com quantas cartas você quer jogar?"));
+    }
+    alert("iniciou o jogo");
 }
 iniciar();
