db923fa4652988fdeb103fc8b9d3e71b931dd625
thaliard <thalia.born@gmail.com>
2020-09-09 18:47:46 -0300

    adicionando função que sorteia os parrots para o jogo com base na quant. de cartas

--- script.js
+++ script.js
@@ -15,6 +15,8 @@ se for a primeira carta do par ela tem que ficar parada até selecionar a segund
     se errar: volta as duas cartas para a posição inicial após 1 segundo
 ao final do jogo informar o numero de jogadas -> numero de cliques / 2
  */
+
+ //-------------------------------------------------------------------------------array com os parrots
 var imgParrots = [
     "imagens/bobrossparrot.gif",
     "imagens/explodyparrot.gif",
@@ -25,6 +27,10 @@ var imgParrots = [
     "imagens/unicornparrot.gif"
 ]
 
+
+ //---------------------------------------------------------------------array com os parrots sorteado pelo total de cartas
+var selecionados = [];
+
 //---------------------------------------------------------------------------------função para o user escolher o nº cartas
 function iniciar(){
     var qtdCartas = parseInt(prompt("Quantas cartas você quer jogar? \n Escolha um nº entre 4 e 14"));
@@ -33,6 +39,25 @@ function iniciar(){
     while((qtdCartas % 2 !== 0) || (qtdCartas < 4) || (qtdCartas > 14)){
         qtdCartas = parseInt(prompt("Você precisa escolher um nº par entre 4 e 14. Com quantas cartas você quer jogar?"));
     }
-    //renderizaCartas(qtdCartas);
+    renderizaCartas(qtdCartas);
 }
 iniciar();
+
+//---------------------------------------------------------------------------------inserir as cartas do jogo
+function renderizaCartas(qtdCartas){
+    var contador = 0;
+    
+    sortearParrots(qtdCartas);
+}
+
+
+//---------------------------------------------------------------------------------sorteando os papagaios
+function sortearParrots(qtdCartas){
+    for(var i = 0; i < qtdCartas/2; i++){
+        var indiceSorteio = Math.floor(imgParrots.length * Math.random());
+        selecionados.push(imgParrots[indiceSorteio]);
+        selecionados.push(imgParrots[indiceSorteio]);
+        imgParrots.splice(indiceSorteio,1);
+    }
+    console.log(selecionados);
+}
