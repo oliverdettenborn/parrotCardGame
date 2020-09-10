6bdde4dcec133b8ddc47b3963ccdf50963eec999
thaliard <thalia.born@gmail.com>
2020-09-10 11:43:37 -0300

    refactoring: dividindo o js em dois arquivos, um para as funções principais do jogo e o segundo para as funções de renderização das cartas em tela

--- js/script.js
+++ js/script.js
@@ -1,5 +1,11 @@
+/*OBSERVAÇÕES
+    - nesse arquivo estão as funções principais do jogo
+    - esse jogo aceita no máximo 14 cartas para o jogador
+    - caso seja necessário aumentar o numero de cartas, deve-se adicionar mais src na array imgParrots no arquivo renderizacao.js
+*/
 
-//---------------------------------------------------------------------------------variaveis globais para controle do jogo
+
+//-------------------------------------------------------------------------variaveis globais para controle do jogo
 var countJogada = 0;
 var countAcerto = 0;
 var cartasViradas = [];
@@ -7,32 +13,7 @@ var acertosParaFinalizar = 0;
 var ul = document.querySelector(".cartas");
 
 
-//-------------------------------------------------------------------------------array com os parrots
-var imgParrots = [
-    "imagens/bobrossparrot.gif",
-    "imagens/explodyparrot.gif",
-    "imagens/fiestaparrot.gif",
-    "imagens/metalparrot.gif",
-    "imagens/revertitparrot.gif",
-    "imagens/tripletsparrot.gif",
-    "imagens/unicornparrot.gif"
-];
-
-
-//---------------------------------------------------------------------array com os parrots sorteado pelo total de cartas
-var selecionados = [];
-
-//---------------------------------------------------------------------------------sorteando os papagaios
-function sortearParrots(qtdCartas){
-    for(var i = 0; i < qtdCartas/2; i++){
-        var indiceSorteio = Math.floor(imgParrots.length * Math.random());
-        selecionados.push(imgParrots[indiceSorteio]);
-        selecionados.push(imgParrots[indiceSorteio]);
-        imgParrots.splice(indiceSorteio,1);
-    }
-}
-
-//---------------------------------------------------------------------------------função para o user escolher o nº cartas
+//-------------------------------------------------------------------------função para o user escolher o nº cartas
 function iniciarJogo(){
     var qtdCartas = parseInt(prompt("Quantas cartas você quer jogar? \n Escolha um nº entre 4 e 14"));
 
@@ -47,39 +28,29 @@ function iniciarJogo(){
 }
 iniciarJogo();
 
-//---------------------------------------------------------------------------------inserir as cartas do jogo
-function renderizaCartas(qtdCartas){
-    sortearParrots(qtdCartas);
-
-    while(selecionados.length !== 0){
-        //---------------------------------------------------------criar uma carta
-        var carta = document.createElement("li");
-
-        //---------------------------------------------------------insererir função onclick na carta
-        carta.setAttribute('onclick','virarCarta(this)');
-
-        //---------------------------------------------------------inserir uma imagem aleatoriamente nela
-        var imgCarta = document.createElement('img');
-
-        var indiceSorteio = Math.floor(selecionados.length * Math.random());
-        
-        var urlAleatorio = selecionados[indiceSorteio];
-        imgCarta.setAttribute('src', urlAleatorio);
-        
-        //-------------------------------------------------remover essa imagem da lista de selecionados para não repetir
-        selecionados.splice(indiceSorteio,1);
-
-        //-------------------------------------------------inserir display none na imagem do parrot
-        imgCarta.style.display = "none";
-
-        //-------------------------------------------------vincular essa imagem ao li da carta criada
-        carta.appendChild(imgCarta);
-
-        //-------------------------------------------------selecionar ul onde estão as cartas e vincular a carta
-        ul.appendChild(carta);
-    };
-    acertosParaFinalizar = qtdCartas;
+//----------------------------------------------------------------------------------- verificando se o jogo acabou
+function finalizaJogo(){
+    setTimeout(function(){
+        alert("Você ganhou em " + countJogada + " jogadas!");
+        reiniciarJogo()
+    }, 500);
 }
+
+//------------------------------------------------------------------------------------- função para reiniciar o jogo
+function reiniciarJogo(){
+    var querReiniciar = prompt("Você quer reiniciar o jogo?");
+
+    while(querReiniciar !== "sim"){
+        querReiniciar = prompt("Para reiniciar o jogo, digite a palavra sim");
+    };
+    
+    resetarJogo();
+    iniciarJogo();
+}
+
+
+
+
 
 
 //---------------------------------------------------------------------------------função que vira a carta ao clicar
@@ -97,7 +68,8 @@ function virarCarta(element){
     };
 }
 
-//-----------------------------------------------------------------------------função que confere a segunda carta clicada
+
+//-----------------------------------------------------------------------função para quando user clica na segunda carta
 function segundoClique(){
     var primeiraCarta = pegaSrc(0);
     var segundaCarta = pegaSrc(1);
@@ -118,42 +90,8 @@ function segundoClique(){
 }
 
 
-//-----------------------------------------------------------------função para pegar o scr da imagem do elemento clicado
-function pegaSrc(i){
-    var carta = cartasViradas[i].querySelector("img");
-    var parrot = carta.getAttribute('src');
-    return parrot;
-}
 
-//---------------------------------------------------------------------------- verificando se as cartas tem o mesmo parrot
-function verificaCartasIguais(primeiraCarta,segundaCarta){
-    if(primeiraCarta === segundaCarta)
-        return true;
-    else
-        return false;
-}
-
-//---------------------------------------------------------------------------- verificando se o jogo acabou
-function finalizaJogo(){
-    setTimeout(function(){
-        alert("Você ganhou em " + countJogada + " jogadas!");
-        reiniciarJogo()
-    }, 500);
-}
-
-//---------------------------------------------------------------------------- função para reiniciar o jogo
-function reiniciarJogo(){
-    var querReiniciar = prompt("Você quer reiniciar o jogo?");
-
-    while(querReiniciar !== "sim"){
-        querReiniciar = prompt("Para reiniciar o jogo, digite a palavra sim");
-    };
-    
-    resetarJogo();
-    iniciarJogo();
-}
-
-//------------------------------------------------------------------------função que reseta a array com os src dos parrot
+//---------------------------------------------------------------------função que reseta a array com os src dos parrot
 function resetarJogo(){
     ul.innerHTML = "";
     
@@ -174,14 +112,36 @@ function resetarJogo(){
     selecionados = [];
 }
 
-//---------------------------------------------------------------------------------mostra verso da carta
+
+
+
+//-----------------------------------------------------------------função para pegar o scr da imagem do elemento clicado
+function pegaSrc(i){
+    var carta = cartasViradas[i].querySelector("img");
+    var parrot = carta.getAttribute('src');
+    return parrot;
+}
+
+//------------------------------------------------------------------------- verificando se as cartas tem o mesmo parrot
+function verificaCartasIguais(primeiraCarta,segundaCarta){
+    if(primeiraCarta === segundaCarta)
+        return true;
+    else
+        return false;
+}
+
+
+
+
+
+//----------------------------------------------------------------------------------------------mostra verso da carta
 function mostraVerso(element){
     element.style.transform = "rotateY(180deg)";
     element.style.backgroundImage = "none";
     var img = element.querySelector('img');
     img.style.display = "initial";
 }
-//---------------------------------------------------------------------------------mostra frente da carta
+//---------------------------------------------------------------------------------------------mostra frente da carta
 function mostraFrente(element){
     element.style.transform = "rotateY(0deg)";
     element.style.backgroundImage = "url(imagens/front.png)";
